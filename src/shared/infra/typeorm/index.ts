e3bdc5689d69547dc2ee80a18fb174f75749d63a
Leonardo <leonardobelilo@outlook.com>
2022-09-03 09:50:14 -0300

    correção de imports

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,27 +3,26 @@ import { DataSource } from "typeorm";
 import { AgentToken } from "../../../modules/accounts/userToken/infra/typeorm/entities/AgentToken";
 import { Action } from "../../../modules/actions/infra/typeorm/entities/Action";
 import { Departament } from "../../../modules/Actions/infra/typeorm/entities/Departament";
-import { Agent } from "../../../modules/agents/infra/typeorm/entities/Agent";
-import { ColabAgent } from "../../../modules/agents/infra/typeorm/entities/ColabAgent";
-import { Interests } from "../../../modules/agents/infra/typeorm/entities/Interests";
-import { JourneyAgent } from "../../../modules/agents/infra/typeorm/entities/JourneyAgent";
+import { Agent } from "../../../modules/Agents/infra/typeorm/entities/Agent";
+import { ColabAgent } from "../../../modules/Agents/infra/typeorm/entities/ColabAgent";
+import { Interests } from "../../../modules/Agents/infra/typeorm/entities/Interests";
+import { JourneyAgent } from "../../../modules/Agents/infra/typeorm/entities/JourneyAgent";
 import { PhotoPublicationAgent } from "../../../modules/Agents/infra/typeorm/entities/PhotoPublicationAgent";
 import { PublicationAgent } from "../../../modules/Agents/infra/typeorm/entities/PublicationAgent";
-import { Skills } from "../../../modules/agents/infra/typeorm/entities/Skills";
-import { SponsorAgent } from "../../../modules/agents/infra/typeorm/entities/SponsorAgent";
-import { AgentMission } from "../../../modules/missions/infra/typeorm/entities/AgentMission";
-import { Mission } from "../../../modules/missions/infra/typeorm/entities/Mission";
-import { SponsorMission } from "../../../modules/missions/infra/typeorm/entities/SponsorMission";
+import { Skills } from "../../../modules/Agents/infra/typeorm/entities/Skills";
+import { SponsorAgent } from "../../../modules/Agents/infra/typeorm/entities/SponsorAgent";
+import { AgentMission } from "../../../modules/Missions/infra/typeorm/entities/AgentMission";
+import { Mission } from "../../../modules/Missions/infra/typeorm/entities/Mission";
+import { SponsorMission } from "../../../modules/Missions/infra/typeorm/entities/SponsorMission";
 import  'dotenv/config';
 import { AgentAction } from "../../../modules/Actions/infra/typeorm/entities/AgentAction";
 import { AgentDepartament } from "../../../modules/Actions/infra/typeorm/entities/AgentDepartament";
 import {  TaskDepartament } from "../../../modules/Actions/infra/typeorm/entities/TaskDepartament";
 
-
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: "localhost",
-  port: 3306,
+  port: 3307,
   username: process.env.USER_DATABASE ,
   password: process.env.PASSWORD_DATABASE,
   database: process.env.DB_NAME, 
